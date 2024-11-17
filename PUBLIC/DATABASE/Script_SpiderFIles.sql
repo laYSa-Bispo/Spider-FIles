0aac0cab5e85538af0bc1c988dc622a2eb7fd7f7
laYSa-Bispo <laysa.silva@sptech.school>
2024-11-17 14:38:35 -0300

    Atualização da Database

--- PUBLIC/DATABASE/Script_SpiderFIles.sql
+++ PUBLIC/DATABASE/Script_SpiderFIles.sql
@@ -7,3 +7,18 @@ Usuario VARCHAR(45),
 Email VARCHAR(100),
 Senha VARCHAR(30)
 );
+
+CREATE TABLE Quiz (
+idQuiz INT PRIMARY KEY AUTO_INCREMENT, 
+fkUsuario INT,
+	CONSTRAINT fkQuizUsuario FOREIGN KEY (fkUsuario)
+		REFERENCES Usuario(idUsuario),
+PerguntaUm TINYINT, 
+PerguntaDois TINYINT, 
+PerguntaTres TINYINT, 
+PerguntaQuatro TINYINT, 
+PerguntaCinco TINYINT, 
+PerguntaSeis TINYINT
+);
+desc usuario;
+select * from usuario;
